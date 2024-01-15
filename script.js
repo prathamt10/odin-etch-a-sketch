69f6461ce5a27a189210ddccdcb439080e80f68a
prathamt10 <prathamtalware11@gmail.com>
2024-01-15 19:43:38 +0530

    minor refactor

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-const DIMENSION = 960;
-let isMouseHolding = false;
+const DIMENSION = 768;
+let isMouseButtonPressed = false;
+let draw_color = 'black';
 
 function startDraw(square) {
-    isMouseHolding = true;
+    isMouseButtonPressed = true;
     draw(square)
 }
 
 function endDraw(square) {
     draw(square);
-    isMouseHolding = false;
+    isMouseButtonPressed = false;
 }
 
 function draw(square) {
-    if (!isMouseHolding) {
+    if (!isMouseButtonPressed) {
         return;
     }
-    square.style.backgroundColor = 'black';
+    square.style.backgroundColor = draw_color;
 }
 
 function createSquares(count) {
     const size = DIMENSION / count;
+    
     for (let i = 0; i < count; i++) {
         const row = document.createElement('div');
         row.style.display = 'flex';
+
         for (let j = 0; j < count; j++) {
             const square = document.createElement('div');
             square.className = 'squares';
@@ -30,9 +33,11 @@ function createSquares(count) {
             square.style.width = `${size}px`;
             square.style.height = `${size}px`;
             square.style.border = '1px solid black';
+
             square.addEventListener('mousedown', (e) => { startDraw(e.target) });
             square.addEventListener('mousemove', (e) => { draw(e.target) });
             square.addEventListener('mouseup', (e) => { endDraw(e.target) });
+
             row.appendChild(square);
         }
         sketchpad.appendChild(row);
@@ -50,8 +55,8 @@ clear_button.addEventListener('click', () => {
     })
 });
 
-const change_grid_size = document.querySelector('#sizes');
-change_grid_size.addEventListener('change', (e) => {
-    sketchpad.replaceChildren();
+const grid_size = document.querySelector('#sizes');
+grid_size.addEventListener('change', (e) => {
+    sketchpad.innerHTML = "";
     createSquares(parseInt(e.target.value));
-})
+});
