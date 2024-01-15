9de5a290d8306563918eb664c97ee4272c82a395
prathamt10 <prathamtalware11@gmail.com>
2024-01-15 17:44:13 +0530

    added the hover effect

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const DIMENSION = 960;
+let isMouseHolding = false;
+
+function startDraw(square) {
+    isMouseHolding = true;
+    draw(square)
+}
+
+function endDraw(square) {
+    draw(square);
+    isMouseHolding = false;
+}
+
+function draw(square) {
+    if (!isMouseHolding) {
+        return;
+    }
+    square.classList.add('selected');
+}
 
 function createSquares(count) {
     const size = DIMENSION / count;
@@ -11,6 +29,9 @@ function createSquares(count) {
             square.style.width = `${size}px`;
             square.style.height = `${size}px`;
             square.style.border = '1px solid black';
+            square.addEventListener('mousedown', (e) => { startDraw(e.target) });
+            square.addEventListener('mousemove', (e) => { draw(e.target) });
+            square.addEventListener('mouseup', (e) => { endDraw(e.target) });
             row.appendChild(square);
         }
         sketchpad.appendChild(row);
@@ -18,4 +39,4 @@ function createSquares(count) {
 }
 
 const sketchpad = document.querySelector(".sketchpad");
-createSquares(16);
+createSquares(32);
